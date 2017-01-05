95e455dab31280d4a0afd13bbad16d6d4957d60d
Stujo <stujo@users.noreply.github.com>
2017-01-05 11:55:32 -0800

    Add Sample Message Toast, Add Progress Pending

--- public/js/init.js
+++ public/js/init.js
@@ -1,8 +1,22 @@
-( function($) {
+( function($, Materialize) {
 $(function() {
+
+  function showProgress(e) {
+    var $button = $(e.target);
+    var $row = $button.closest('.long-job-button-row');
+    $row.find('button').hide();
+    $row.find('.show-progress').show();
+  }
 
   $('.button-collapse').sideNav();
   $('select').material_select();
 
+  $('.long-job-button-row button').click(showProgress);
+
+
+  if (typeof toastMessage !== 'undefined') {
+    Materialize.toast(toastMessage, 4000);
+  }
+
 });
-} )(jQuery);
+} )(jQuery, Materialize);
